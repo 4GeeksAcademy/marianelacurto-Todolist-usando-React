9ab52d5f338f4fa869ae881bd92031c0b004d6bd
marianelacurto <marianelacurto@gmail.com>
2026-06-09 09:59:38 +0000

    termine la lista

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState(["Make the bed", "Wash my hands", "Eat", "Walk the dog"]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const addTask = () => {
+		if (task.trim() !== "") {
+			setTasks([...tasks, task]);
+			setTask("");
+		}
+	};
+	const deleteTask = (indexToDelete) => {
+		const newTasks = tasks.filter((task, index) => {
+			return index !== indexToDelete;
+		});
+		setTasks(newTasks);
+	};
+
+	return (
+		<div className="todo-container">
+			<h1>todos</h1>
+			<div className="todo-box">
+
+				<div className="input-box">
+					<input type="text" placeholder="What needs to be done?" value={task} onChange={(event) => setTask(event.target.value)}>
+					</input>
+					<button onClick={addTask}>+</button>
+				</div>
+				<ul>
+					{tasks.length === 0 ? (
+						<li className="empty-message">No hay tareas, agregar tareas</li>
+					) : (
+						tasks.map((task, index) => {
+							return (
+								<li key={index} className="todo-item"><span>{task}</span>
+									<button className="delete-btn" onClick={() => deleteTask(index)}>
+										x
+									</button>
+								</li>
+							);
+						})
+					)}
+				</ul>
+				<div className="todo-footer">{tasks.length} item left
+				</div>
+			</div>
 		</div>
 	);
 };
